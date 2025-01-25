588ec6b5d9d6d9f38864f1ebb370aa21dd8ab551
nuke cola <manaska129@gmail.com>
2025-01-25 10:13:04 +0700

    test_

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/testmain.tscn")
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 
 func _on_hint_button_pressed() -> void:
